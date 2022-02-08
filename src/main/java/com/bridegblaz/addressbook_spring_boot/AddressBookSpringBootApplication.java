38e08cf9640fb2b93cc6977c90311c9118061130
Achyuth Naidu <ashwath.bly@gamil.com>
2022-02-08 12:56:58 +0530

    Added : Create an Address Book Spring Project to cater to REST Request from Address Book UI

---
+++ src/main/java/com/bridegblaz/addressbook_spring_boot/AddressBookSpringBootApplication.java
@@ -0,0 +1,23 @@
+package com.bridegblaz.addressbook_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author -> Ashwath Naidu <ashwath.bly@gmail.com>
+ * openjdk version "11.0.12" 2021-07-20
+ * OpenJDK Runtime Environment 18.9 (build 11.0.12+7)
+ * OpenJDK 64-Bit Server VM 18.9 (build 11.0.12+7, mixed mode)
+ *
+ * -----------------------------------------------------------------------------------------------------------------------------
+ *
+ * UC1 : Create an Address Book Spring Project to cater to REST Request from Address Book UI - Make sure
+ *
+ */
+@SpringBootApplication
+public class AddressBookSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookSpringBootApplication.class, args);
+    }
+}
